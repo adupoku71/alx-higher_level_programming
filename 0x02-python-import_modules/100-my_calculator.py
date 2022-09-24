7bae4499a3cab9b50070e30894a4ea933de8685b
adupoku71 <davidpoku71@gmail.com>
2022-09-23 17:51:57 -0700

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from sys import argv as args
+from calculator_1 import add, sub, mul, div
+
+
+if __name__ == '__main__':
+    print(args)
+    op_dict = {
+            '+': add,
+            '-': sub,
+            '/': div,
+            '*': mul
+    }
+
+    if len(args) - 1 != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    else:
+        if args[2] not in ('+', '-', '/', '*'):
+            print('Unknown operator. Available operators: +, -, * and /')
+        else:
+            a = int(args[1])
+            b = int(args[3])
+            op = args[2]
+            x = op_dict[op](a, b)
+
+            print('{:d} {} {:d} = {:d}'.format(a, op, b, x))
