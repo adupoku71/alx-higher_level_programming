40ce0f5b95daca4467a668f199844114a5c53d27
adupoku71 <davidpoku71@gmail.com>
2022-09-24 03:04:41 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,13 +6,10 @@ def print_list_integer(my_list=[]):
 
     Args:
         my_list: list of integers
-    
+
     Returns:
         None
 
     """
     for x in my_list:
         print('{:d}'.format(x))
-
-
-print_list_integer([1, 2, 3, 4, 5])
