a5ebfa4912119dd6639e54f2bcfea74c76cb58fb
adupoku71 <davidpoku71@gmail.com>
2022-09-25 04:12:17 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    """
+    returns the biggest integer of a list.
+    """
+
+    if not my_list:
+        return None
+
+    my_list.sort()
+
+    return my_list[len(my_list) - 1]
