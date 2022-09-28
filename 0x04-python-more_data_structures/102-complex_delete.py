63290cc29bfc857b61576465eee37b72366ada85
adupoku71 <davidpoku71@gmail.com>
2022-09-28 15:42:35 -0700

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+
+    for key, data in a_dictionary.copy().items():
+        if data == value:
+            del a_dictionary[key]
+
+    return a_dictionary
+
