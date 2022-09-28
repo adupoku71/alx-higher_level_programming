17cdc6ba0e7e86a8b0c739a8b5c19fb3f0a8ff28
adupoku71 <davidpoku71@gmail.com>
2022-09-28 15:12:19 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    """
+    returns a key with the biggest integer value
+    """
+    max_val = 0
+    winner = None
+    if type(a_dictionary) is dict:
+        for key, value in a_dictionary.items():
+            if value > max_val:
+                max_val = value
+                winner = key
+    return winner
