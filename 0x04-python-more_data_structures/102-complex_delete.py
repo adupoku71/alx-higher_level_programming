d6761fa31fd4425907b6adf1166a47206d48afb5
adupoku71 <davidpoku71@gmail.com>
2022-09-28 15:47:50 -0700

    removed redundant line

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,9 +3,11 @@
 
 def complex_delete(a_dictionary, value):
 
+    if not a_dictionary:
+        return None
+
     for key, data in a_dictionary.copy().items():
         if data == value:
             del a_dictionary[key]
 
     return a_dictionary.copy()
-
